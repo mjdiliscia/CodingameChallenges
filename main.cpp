7acbb8c4ca89a643da4183e593b5dae944ad5ecf
Martin Javier Di Liscia <mjdiliscia@gmail.com>
2022-12-18 14:17:11 -0300

    Big refactor to use vectors and iterators

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 const int MAX_WIDTH = 15;
 const int MAX_HEIGHT = 7;
-constexpr int MAX_TILES = 15*7;
+constexpr int MAX_TILES = MAX_WIDTH * MAX_HEIGHT;
 const int MAX_ACTIONS = 100;
 
 using Coord = tuple<int, int>;
@@ -20,6 +20,8 @@ int y(Coord coord) { return get<1>(coord); }
 Coord coord(int x, int y) { return make_tuple(x, y); }
 
 struct Tile {
+    Tile() { neighbors.reserve(4); }
+
     int scrapAmount;
     int owner;
     int units;
@@ -27,33 +29,38 @@ struct Tile {
     int canBuild;
     int canSpawn;
     int willBeScrapped;
+    Coord coord;
+    vector<vector<Tile>::iterator> neighbors;
 };
-using TileRow = array<Tile, MAX_WIDTH>;
-using TileMatrix = array<TileRow, MAX_HEIGHT>;
+using TileRow = vector<Tile>;
+using TileMatrix = vector<TileRow>;
 
 struct RobotTile {
-    Coord coord;
+    RobotTile(TileRow::iterator tile, int robots) : tile(tile), robots(robots) {}
+    TileRow::iterator tile;
     int robots;
 };
-using RobotTiles = array<RobotTile, MAX_TILES>;
+using RobotTiles = vector<RobotTile>;
 
 enum class ACTION {
     MOVE,
     BUILD,
     SPAWN,
-    WAIT
+    WAIT,
+    MESSAGE,
+    Count
 };
-array<char[8], 5> actionToString{"MOVE", "BUILD", "SPAWN", "WAIT"};
+array<char[8], static_cast<size_t>(ACTION::Count)> actionToString{"MOVE", "BUILD", "SPAWN", "WAIT", "MESSAGE"};
 struct Action {
     ACTION kind;
     int amount;
     Coord pos;
     Coord target;
 };
-using Actions = array<Action, MAX_ACTIONS>;
+using Actions = vector<Action>;
 
 enum class DIRECTION {
-    UP, DOWN, LEFT, RIGHT
+    UP, DOWN, LEFT, RIGHT, Count
 };
 
 int boardWidth;
@@ -62,24 +69,25 @@ int currentMatter;
 int opponentMatter;
 TileMatrix board;
 RobotTiles ownRobotsTiles;
-int ownRobotTilesNum;
 RobotTiles opponentRobotsTiles;
-int opponentRobotTilesNum;
 Actions nextActions;
-int nextActionsNum;
 
 minstd_rand randomEngine = minstd_rand(random_device()());
-uniform_int_distribution directionGenerator = uniform_int_distribution(0, 3);
+uniform_int_distribution<int> directionGenerator[4] = {
+    uniform_int_distribution(0, 0),
+    uniform_int_distribution(0, 1),
+    uniform_int_distribution(0, 2),
+    uniform_int_distribution(0, 3)
+};
 
 void init();
 void updateGameStatus();
-void addAction(Action action);
 void calculateOrders();
 void sendOrders();
-void moveByRandomWalk(Tile tile, Coord coordinate);
-void moveByRandomWalk(RobotTile tile);
-Tile& coordTile(Coord coord);
-tuple<bool, Coord> getNeighbor(Coord coord, DIRECTION direction);
+void moveByRandomWalk(Tile& tile);
+void moveByRandomWalk(RobotTile& tile);
+TileRow::iterator coordTile(const Coord& coord);
+tuple<bool, Coord> getNeighbor(const Coord& coord, DIRECTION direction);
 
 int main()
 {
@@ -94,13 +102,23 @@ int main()
 
 void init() {
     cin >> boardWidth >> boardHeight; cin.ignore();
+    board.reserve(boardHeight);
+    board.resize(boardHeight);
+    for (auto& row : board) {
+        row.reserve(boardWidth);
+        row.resize(boardWidth);
+    }
+    ownRobotsTiles.reserve(MAX_TILES);
+    opponentRobotsTiles.reserve(MAX_TILES);
+    nextActions.reserve(MAX_ACTIONS);
 }
 
 void updateGameStatus() {
-    ownRobotTilesNum = opponentRobotTilesNum = 0;
+    ownRobotsTiles.clear();
+    opponentRobotsTiles.clear();
     cin >> currentMatter >> opponentMatter; cin.ignore();
-    for (int i = 0; i < boardHeight; i++) {
-        for (int j = 0; j < boardWidth; j++) {
+    for (int i = 0; i < board.size(); i++) {
+        for (int j = 0; j < board[i].size(); j++) {
             Tile& tile = board[i][j];
             cin >> tile.scrapAmount;
             cin >> tile.owner;
@@ -111,43 +129,54 @@ void updateGameStatus() {
             cin >> tile.willBeScrapped;
             cin.ignore();
 
+            tile.coord = coord(j, i);
             assert(tile.units == 0 || tile.owner != -1);
             if (tile.units > 0) {
-                auto& robotTile = tile.owner == 1 ? ownRobotsTiles[ownRobotTilesNum++] : opponentRobotsTiles[opponentRobotTilesNum++];
-                robotTile.coord = coord(j, i);
-                robotTile.robots = tile.units;
+                RobotTile robotTile(board[i].begin() + j, tile.units);
+                if (tile.owner == 1) ownRobotsTiles.push_back(robotTile);
+                else opponentRobotsTiles.push_back(robotTile);
+            }
+        }
+    }
+
+    for (int i = 0; i < board.size(); i++) {
+        for (int j = 0; j < board[i].size(); j++) {
+            Tile& tile = board[i][j];
+            tile.neighbors.clear();
+
+            for (int d = 0; d < static_cast<int>(DIRECTION::Count); d++) {
+                DIRECTION direction = static_cast<DIRECTION>(d);
+                auto [valid, coord] = getNeighbor(tile.coord, direction);
+                if (valid) {
+                    tile.neighbors.push_back(coordTile(coord));
+                }
             }
         }
     }
 }
 
-void addAction(Action action) {
-    nextActions[nextActionsNum++] = action;
-}
-
 void calculateOrders() {
-    for (int i=0; i < ownRobotTilesNum; i++) {
-        moveByRandomWalk(ownRobotsTiles[i]);
+    for (auto robotsTile : ownRobotsTiles) {
+        moveByRandomWalk(robotsTile);
     }
 }
 
 void sendOrders() {
-    if (nextActionsNum == 0) {
-        cout << "WAIT";
-    }
-    for (int i = 0; i < nextActionsNum; i++) {
-        Action& action = nextActions[i];
+    if (nextActions.size() == 0) cout << "WAIT";
+    for (auto& action : nextActions) {
         cout << actionToString[static_cast<int>(action.kind)];
         switch (action.kind) {
             case ACTION::MOVE:
             case ACTION::SPAWN:
                 cout << " " << action.amount;
+            default: ;
         }
         switch (action.kind) {
             case ACTION::MOVE:
             case ACTION::BUILD:
             case ACTION::SPAWN:
                 cout << " " << x(action.pos) << " " << y(action.pos);
+            default: ;
         }
         if (action.kind == ACTION::MOVE) {
             cout << " " << x(action.target) << " " << y(action.target);
@@ -155,31 +184,44 @@ void sendOrders() {
         cout << ";";
     }
     cout << endl;
-    nextActionsNum = 0;
+    nextActions.clear();
 }
 
-void moveByRandomWalk(Tile tile, Coord coordinate) {
-    assert(tile.owner == 1);
-    moveByRandomWalk(RobotTile{coordinate, tile.units});
-}
+void moveByRandomWalk(Tile& tile) {
+    assert(tile.owner == 1 && tile.units > 0);
 
-void moveByRandomWalk(RobotTile tile) {
-    int directionAmount[4]{0,0,0,0};
-    for (int i = 0; i < tile.robots; i++) {
-        directionAmount[directionGenerator(randomEngine)]++;
+    int neighborsCount = tile.neighbors.size();
+    vector<tuple<int, Coord>> moves;
+    moves.resize(neighborsCount);
+    for (int i = 0; i < tile.units; i++) {
+        int neighborNum = directionGenerator[neighborsCount-1](randomEngine);
+        auto& move = moves[neighborNum];
+        get<0>(move)++;
+        get<1>(move) = tile.neighbors[neighborNum]->coord;
     }
 
-    for (int i = 0; i < 4; i++) {
-        if (directionAmount[i] > 0) addAction(Action{ACTION::MOVE, directionAmount[i], tile.coord, get<1>(getNeighbor(tile.coord, static_cast<DIRECTION>(i)))});
+    for (auto move : moves) {
+        if (get<0>(move) > 0) {
+            nextActions.push_back(Action{
+                ACTION::MOVE,
+                get<0>(move),
+                tile.coord,
+                get<1>(move)
+            });
+        }
     }
 }
 
-Tile& coordTile(Coord coord) {
-    return board[y(coord)][x(coord)];
+void moveByRandomWalk(RobotTile& tile) {
+    moveByRandomWalk(*tile.tile);
+}
+
+TileRow::iterator coordTile(const Coord& coord) {
+    return board[y(coord)].begin() + x(coord);
 }
 
 #define BORDER_MOVE(v, c, m) if (v != c) { v += m; differentCoord = true; }
-tuple<bool, Coord> getNeighbor(Coord coordinate, DIRECTION direction) {
+tuple<bool, Coord> getNeighbor(const Coord& coordinate, DIRECTION direction) {
     auto [x, y] = coordinate;
     bool differentCoord = false;
     switch (direction) {
@@ -194,6 +236,7 @@ tuple<bool, Coord> getNeighbor(Coord coordinate, DIRECTION direction) {
             break;
         case DIRECTION::RIGHT:
             BORDER_MOVE(x, boardWidth-1, 1);
+        default: ;
     }
     return make_tuple(differentCoord, coord(x, y));
 }
